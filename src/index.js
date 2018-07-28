9aa176b28dedc03d8041e7a56af99dd3b6f9ee1a
Alexander Persky <info@alexanderpersky.com>
2018-07-27 17:10:34 -0700

    Misc.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker'; // TODO: Is this needed for this app?
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
@@ -10,3 +10,4 @@ registerServiceWorker();
 // TODO: Review the project structure. Does this follow the best practice for organizing a React app?
 // TODO: Set up ESLint, preferably with the most common settings or config for a React project.
 // TODO: Examine the manifest.json and index.html, and update them accordingly.
+// TODO: Dump the React Bootstrap in favor of Tailwind.css. Good to get the experience. This will force me to create more components.
